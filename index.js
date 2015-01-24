a98034fde567aec0d77fa6df5f43a77b8060e432
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-23 20:55:12 -0500

    update based on benchmarks.
    (30% faster with slice when trailing slash is stripped)

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 /*!
  * normalize-path <https://github.com/jonschlinkert/normalize-path>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
-'use strict';
-
-module.exports = function(filepath, stripTrailing) {
-  filepath = filepath
-    .replace(/[\\\/]+/g, '/')
-    .toLowerCase();
-
+module.exports = function(fp, stripTrailing) {
+  fp = fp.replace(/[\\\/]+/g, '/');
   if (stripTrailing === false) {
-    return filepath;
+    return fp;
   }
-  return filepath.replace(/\/$/g, '');
+
+  return fp.slice(-1) === '/'
+    ? fp.slice(0, fp.length -1)
+    : fp;
 };
