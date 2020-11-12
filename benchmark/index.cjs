0e4b08ca041348c1ad3a95006421ea5cb4901fe9
Daniel Xu <dxu23@uic.edu>
2020-11-12 01:19:24 -0600

    Changed benchmark files' extension to cjs
    For some reason, I couldn't get the benchmark files to
    even run. Not sure if it really matters to the original
    authors though.

--- benchmark/index.cjs
+++ benchmark/index.cjs
@@ -5,7 +5,7 @@ const suite = require('benchmarked');
 const argv = process.argv.slice(2);
 const code = argv[0] || '{current,v2}';
 
-suite.run({ code: `code/${code}.js`, fixtures: 'fixtures/*.js' })
+suite.run({ code: `code/${code}.cjs`, fixtures: 'fixtures/*.cjs' })
   .then(function(stats) {
     console.log(suite.render(stats));
   })
